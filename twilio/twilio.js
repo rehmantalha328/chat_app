99b7eef623d3a09a6143a4b1b46ae210e353b2f6
Mirz4 <ali561642@gmail.com>
2022-04-08 10:59:56 +0500

    new commit

---
+++ twilio/twilio.js
@@ -0,0 +1,25 @@
+const { getEnv } = require("../config");
+
+const accountSid = getEnv("TWILIO_ACCOUNT_SID");
+const authToken = getEnv("TWILIO_AUTH_TOKEN");
+const myNumber = getEnv("MY_NUMBER");
+const client = require("twilio")(accountSid, authToken);
+
+const send_message = ({ body, number }) => {
+  try {
+    // return true;
+    client.messages.create({
+      body,
+      from: "DEFIGRAM",
+      to: number,
+    });
+  } catch (err) {
+    return client.messages.create({
+      body,
+      from: myNumber,
+      to: number,
+    });
+  }
+};
+
+module.exports = { send_message };
