7f44899fd311a7d63b3964740ee28d11e17c527a
zeeshan006 <javeedzeeshan03@gmail.com>
2022-06-07 09:51:10 +0500

    new commit

--- middleWares/media.js
+++ middleWares/media.js
@@ -13,7 +13,7 @@ const multerStorage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         const ext = file.mimetype.split("/")[1];
-        cb(null, `${v4()}-${Date.now()}.${ext}`);
+        cb(null, `${file.originalname}`);
     },
 });
     
