a73466a8b52dfc2563c4589cb8474d8525541b1a
Mirz4 <ali561642@gmail.com>
2022-07-29 16:15:36 +0500

    new commit

--- routes/admin/allRoutes.js
+++ routes/admin/allRoutes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const trimRequest = require("trim-request");
+const { getSuccessData } = require("../../helper_functions/helpers");
 const Prisma_Client = require("../../prisma_client/_prisma");
 const prisma = Prisma_Client.prismaClient;
 
 router.get("/getAllRegisteredUsers", trimRequest.all, async (req, res) => {
   try {
+    console.log("here");
     let { user_id } = req.user;
     let admin_id = user_id;
     const getAllUsers = await prisma.user.findMany({
@@ -38,6 +40,7 @@ router.get("/getAllRegisteredUsers", trimRequest.all, async (req, res) => {
         },
       },
     });
+    return res.status(200).send(getSuccessData(getAllUsers));
   } catch (error) {
     if (error && error.message) {
       return res.status(404).send(getError(error.message));
